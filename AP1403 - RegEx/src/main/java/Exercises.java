88df3550a091bc3fc9130697ebb8ea265c9ec7d4
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2025-02-10 23:40:32 +0330

    feat(exercise): designed the problems

---
+++ AP1403 - RegEx/src/main/java/Exercises.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Exercises {
+
+    /*
+        complete the method below, so it will validate an email address
+     */
+    public boolean validateEmail(String email) {
+        String regex = ""; // todo
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
+
+    /*
+        this method should find a date in string
+        note that it should be in british or american format
+     */
+    public String findDate(String string) {
+        // todo
+        return null;
+    }
+
+    /*
+        given a string, implement the method to detect all valid passwords
+        then, it should return the count of them
+
+        a valid password has the following properties:
+        - at least 8 characters
+        - has to include at least one uppercase letter, and at least a lowercase
+        - at least one number and at least a special char "!@#$%^&*"
+        - has no white-space in it
+     */
+    public int findValidPasswords(String string) {
+        // todo
+        return -1;
+    }
+
+    /*
+        you should return a list of *words* which are palindromic
+        by word we mean at least 3 letters with no whitespace in it
+     */
+    public List<String> findPalindromes(String string) {
+        List<String> list = new ArrayList<>();
+        // todo
+        return list;
+    }
+
+    public static void main(String[] args) {
+        // you can test your code here
+    }
+}
